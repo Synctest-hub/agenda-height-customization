75814c767f8c1511282b1eb4d495ae37c8bc8005
Nijamudeen Mohamed Sulaiman <nijamudeensulaiman@syncfusion.com>
2020-01-08 16:52:25 +0530

    Agenda view customization sample added

---
+++ AgendaHeightCustomization/AgendaHeightCustomization/Behavior/ScheduleBehavior.cs
@@ -0,0 +1,40 @@
+﻿using Syncfusion.SfSchedule.XForms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace AgendaHeightCustomization
+{
+    public class ScheduleBehavior : Behavior<ContentPage>
+    {
+        SfSchedule schedule;
+        protected override void OnAttachedTo(ContentPage bindable)
+        {
+            base.OnAttachedTo(bindable);
+            this.schedule = bindable.FindByName<SfSchedule>("schedule");
+            this.schedule.SizeChanged += OnScheduleSizeChanged;
+            this.schedule.MonthViewSettings.ShowAgendaView = true;
+        }
+
+        private void OnScheduleSizeChanged(object sender, EventArgs e)
+        {
+            var scheduler = sender as SfSchedule;
+            if (scheduler.Width > scheduler.Height && Device.RuntimePlatform != Device.UWP)
+            {
+                schedule.MonthViewSettings.AgendaViewHeight = schedule.Height * 0.3;
+            }
+            else
+            {
+                schedule.MonthViewSettings.AgendaViewHeight = schedule.Height * 0.6;
+            }
+        }
+
+        protected override void OnDetachingFrom(ContentPage bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            this.schedule.SizeChanged -= OnScheduleSizeChanged;
+            this.schedule = null;
+        }
+    }
+}
